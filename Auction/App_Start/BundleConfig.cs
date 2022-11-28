80b37740f02c6c1e7e202dd03da4bfdc94ed0df2
Alexey strelets <streletswork@gmail.com>
2022-11-28 20:08:34 +0300

    Add project files

---
+++ Auction/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace OnlineAuctionProject
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/jquery.validate.unobtrusive.min.js"
+                        ));
+
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      Resources.Resource.BootstrapScripts,
+                      "~/Scripts/respond.js"));
+
+        }
+    }
+}
